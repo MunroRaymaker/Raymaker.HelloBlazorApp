d293ba050115d16b3548680d6c60890a8fd5e39e
Jens Rud Nielsen <jenrud@gmail.com>
2021-08-03 09:32:20 +0200

    JSInterOp

---
+++ Raymaker.HelloBlazorApp/wwwroot/js/interop.js
@@ -0,0 +1,15 @@
+﻿function showAlert() {
+    alert("This is an alert from JS Interop");
+}
+
+function createPrompt(question) {
+    return prompt(question)
+}
+
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+function focusOnElement(element) {
+    element.focus();
+}
